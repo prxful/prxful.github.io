a0b38c0693d5a7c5841e116d20140a867477c6ab
buddy <localhost@buddy>
2024-03-28 22:52:21 +0530

    test comehting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="text-center">
-      <h1 className="text-3xl font-bold underline text-blue-500">
+      <h1 className="text-3xl font-bold text-red-500">
         Welcome Buddy
       </h1>
     </div>
