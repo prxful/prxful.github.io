26b591e822a87c08d47a921b5d4f7b06ec9d9e00
buddy <localhost@buddy>
2025-01-27 14:07:17 +0530

    source init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        Welcome Buddy
+      </h1>
+    </div>
+  );
+}
+
+export default App;
